5b01d358e1d517c0cf0f9f5fdcd6450a523eb32f
songhyunsik <cutaandcrazy@gmail.com>
2024-07-02 16:13:07 +0900

    Code

--- Socket/Client.c
+++ Socket/Client.c
@@ -1,60 +1,110 @@
-// 라즈베리파이 5 클라이언트 코드
+// gcc -o client Client.c -lws2811 -lm
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include "ws2811.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
+#define LED_PIN 18
+#define LED_COUNT 1
+#define TARGET_FREQ WS2811_TARGET_FREQ
+#define GPIO_PIN 4
+#define DMA 10
+
+ws2811_t ledstring = {
+    .freq = TARGET_FREQ,
+    .dmanum = DMA,
+    .channel = {
+        [0] = {
+            .gpionum = GPIO_PIN,
+            .count = LED_COUNT,
+            .invert = 0,
+            .brightness = 255,
+            .strip_type = WS2811_STRIP_GRB,
+        },
+    },
+};
+
+// LED 초기화 함수
+void initialize_led() {
+    if (ws2811_init(&ledstring)) {
+        fprintf(stderr, "ws2811_init failed\n");
+        exit(1);
+    }
+}
+
+// LED 해제 함수
+void finalize_led() {
+    ws2811_fini(&ledstring);
+}
+
+// LED 상태 설정 함수
+void set_sk6812_state(const char* command) {
+    if (strcmp(command, "ON") == 0) {
+        printf("SK6812 LED is ON\n");
+        ledstring.channel[0].leds[0] = 0x00FF00; // Green color
+        ws2811_render(&ledstring);
+    } else if (strcmp(command, "OFF") == 0) {
+        printf("SK6812 LED is OFF\n");
+        ledstring.channel[0].leds[0] = 0x000000; // Off
+        ws2811_render(&ledstring);
+    } else {
+        int brightness = atoi(command);
+        printf("Setting SK6812 LED brightness to %d\n", brightness);
+        ledstring.channel[0].brightness = brightness;
+        ws2811_render(&ledstring);
+    }
+}
 
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char data[BUFFER_SIZE] = {0};
-    int brightness = 0;
+    char buffer[BUFFER_SIZE] = {0};
 
-    // 소켓 파일 설명자 생성
+    initialize_led();
+
+    // 소켓 파일 디스크립터 생성
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n 소켓 생성 에러 \n");
+        printf("Socket creation error\n");
+        finalize_led();
         return -1;
     }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
 
-    // 서버 주소 설정
+    // 서버 주소 변환
     if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        printf("\n 유효하지 않은 주소 / 지원되지 않는 주소 \n");
+        printf("Invalid address/ Address not supported\n");
+        finalize_led();
         return -1;
     }
 
     // 서버에 연결 요청
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\n 연결 실패 \n");
+        perror("Connection Failed");
+        finalize_led();
         return -1;
     }
 
-    // 1초마다 서버로부터 데이터 수신 및 조절할 밝기 값을 서버로 전송
+    printf("Connected to the server\n");
+
+    // 실시간 명령 수신 루프
     while (1) {
-        printf("0에서 100 사이의 밝기 값을 입력하세요: ");
-        scanf("%d", &brightness);
-        printf("\n");
-
-        // 밝기 값 전송
-        snprintf(data, sizeof(data), "%d", brightness);
-        send(sock, data, strlen(data), 0);
-
-        // 서버로부터 데이터 수신
-        memset(data, 0, BUFFER_SIZE); // 버퍼 초기화
-        int valread = read(sock, data, BUFFER_SIZE);
+        memset(buffer, 0, BUFFER_SIZE);
+        int valread = read(sock, buffer, BUFFER_SIZE);
         if (valread > 0) {
-            printf("현재 LED의 밝기 데이터를 받았습니다...%s\n", data);
+            buffer[valread] = '\0';
+            set_sk6812_state(buffer); // 명령에 따라 LED 제어
         }
-        sleep(1); // 1초 대기
     }
 
+    // 소켓 종료
     close(sock);
+    finalize_led();
     return 0;
 }
--- Socket/Server.c
+++ Socket/Server.c
@@ -1,130 +1,71 @@
-// 라즈베리파이 제로2 W 서버 코드
-// gcc -o server Server.c -lws2811 -lm
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <time.h>
-#include <stdint.h>
-#include "ws2811.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
-#define LED_COUNT 1
-
-#define TARGET_FREQ WS2811_TARGET_FREQ
-#define GPIO_PIN 18
-#define DMA 10
-#define STRIP_TYPE WS2811_STRIP_GRB
-
-ws2811_t ledstring = {
-    .freq = TARGET_FREQ,
-    .dmanum = DMA,
-    .channel = {
-        [0] = {
-            .gpionum = GPIO_PIN,
-            .count = LED_COUNT,
-            .invert = 0,
-            .brightness = 255,
-            .strip_type = STRIP_TYPE,
-        },
-    },
-};
-
-// void SendToGoogleC(const char* project_id, const char* topic_id, const char* message) {
-//     char command[BUFFER_SIZE];
-//     snprintf(command, sizeof(command), "python3 send_to_pubsub.py %s %s \"%s\"", project_id, topic_id, message);
-//     system(command);
-// }
-
-void adjust_led_brightness(int brightness) {
-    // LED 밝기 조절 코드 추가
-    ws2811_led_t color = (brightness << 16) | (brightness << 8) | brightness;
-    ledstring.channel[0].leds[0] = color;
-    ws2811_render(&ledstring);
-    printf("LED 밝기를 %d로 조절합니다.\n", brightness);
-}
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char data[BUFFER_SIZE] = {0};
-    int brightness = 0;
+    char buffer[BUFFER_SIZE] = {0};
 
-    // 구글 클라우드 프로젝트 ID 및 토픽 ID 설정
-    // const char* project_id = "your_project_id";
-    // const char* topic_id = "your_topic_id";
-
-    // 소켓 파일 설명자 생성
+    // 소켓 파일 디스크립터 생성
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("소켓 실패");
+        perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // 주소 설정
+    // 주소 구조체 설정
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    // 소켓을 해당 포트에 바인딩
+    // 소켓을 주소에 바인딩
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("바인딩 실패");
+        perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    // 연결 대기
+    // 연결 요청 대기
     if (listen(server_fd, 3) < 0) {
-        perror("연결 거부");
+        perror("listen failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("%d포트 연결 대기중입니다...\n", PORT);
-
-    // LED 초기화
-    if (ws2811_init(&ledstring)) {
-        fprintf(stderr, "ws2811_init failed\n");
-        return 1;
-    }
+    printf("Server listening on port %d\n", PORT);
 
     // 연결 수락
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("연결 실패");
+        perror("accept failed");
         close(server_fd);
-        ws2811_fini(&ledstring);
         exit(EXIT_FAILURE);
     }
 
-    // 1초마다 밝기 값 전송 및 클라이언트로부터 밝기 값 수신
+    printf("Connection accepted\n");
+
+    // 실시간 명령 전송 루프
     while (1) {
-        // 클라이언트로부터 밝기 값 수신
-        memset(data, 0, BUFFER_SIZE);
-        int valread = read(new_socket, data, BUFFER_SIZE);
-        if (valread > 0) {
-            brightness = atoi(data); // 문자열을 정수로 변환
-            if (brightness >= 0 && brightness <= 100) {
-                adjust_led_brightness(brightness);
-            }
+        printf("Enter command (ON/OFF) or brightness value (0-255): ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        buffer[strcspn(buffer, "\n")] = 0;  // 개행 문자 제거
+
+        if (strcmp(buffer, "ON") == 0 || strcmp(buffer, "OFF") == 0 || 
+            (atoi(buffer) >= 0 && atoi(buffer) <= 255)) {
+            send(new_socket, buffer, strlen(buffer), 0);
+            printf("Sent command: %s\n", buffer);
+        } else {
+            printf("Invalid input. Please enter 'ON', 'OFF', or a brightness value between 0 and 255.\n");
         }
-
-        // 현재 밝기 값 전송
-        brightness = (brightness + 10) % 100; // 밝기 값 업데이트 (예시로 0에서 90까지 10씩 증가)
-        snprintf(data, sizeof(data), "Brightness: %d", brightness);
-        send(new_socket, data, strlen(data), 0);
-        printf("밝기 데이터를 전송합니다...%s\n", data);
-  
-        // SendToGoogleC(project_id, topic_id, data);
-
-        sleep(1); // 1초 대기
     }
 
+    // 소켓 종료
     close(new_socket);
     close(server_fd);
-    ws2811_fini(&ledstring);
     return 0;
 }
