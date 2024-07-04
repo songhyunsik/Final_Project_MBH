1bc13363c35f4bd32b8717ff11cb4c772fa329cd
Jobokuen <160800224+Jobokuen@users.noreply.github.com>
2024-07-04 11:51:46 +0900

    Update main.c

--- DHT22/main.c
+++ DHT22/main.c
@@ -18,7 +18,7 @@
 #include <unistd.h>
 
 #define MAX_TIMINGS 85
-#define DHT_PIN     0  /* GPIO-11 */
+#define DHT_PIN     0  /* BCM-11 */
 #define SERVER_PORT 8080
 
 int data[5] = { 0, 0, 0, 0, 0 };
