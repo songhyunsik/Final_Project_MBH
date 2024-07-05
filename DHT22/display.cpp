b6c883786486b1e5184a44204fd1916533efe59b
Jobokuen <typhoon1019@gmail.com>
2024-07-05 11:24:50 +0900

    display_dht22

--- DHT22/display.cpp
+++ DHT22/display.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/freetype.hpp>
 #include <ctime>
 #include <locale.h>
+#include "DHT22.h"
+
 
 void renderText(cv::Ptr<cv::freetype::FreeType2> &ft2, cv::Mat &img, const std::string &text, cv::Point pos, int fontHeight, cv::Scalar color) {
     ft2->putText(img, text, pos, fontHeight, color, -1, cv::LINE_AA, true);
@@ -38,6 +40,9 @@ int main() {
 
         char date_str[100];
         char time_str[100];
+        char temp_str[100] = sprintf(temp_str,"%0.1f",dhtData->temperature);
+        char humi_str[100] = sprintf(humi_str,"%0.1f",dhtData->humidity);
+    }
         snprintf(date_str, sizeof(date_str), "%04d-%02d-%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
         snprintf(time_str, sizeof(time_str), "%02d:%02d %s", tm.tm_hour % 12 == 0 ? 12 : tm.tm_hour % 12, tm.tm_min, tm.tm_hour >= 12 ? "PM" : "AM");
 
@@ -50,8 +55,8 @@ int main() {
         renderText(ft2, display, "Room", cv::Point(850, 50), 50, cv::Scalar(255, 255, 255));
         renderText(ft2, display, "ON", cv::Point(850, 100), 50, cv::Scalar(255, 255, 255));
         renderText(ft2, display, "OFF", cv::Point(950, 100), 50, cv::Scalar(255, 255, 255));
-        renderText(ft2, display, "25도", cv::Point(850, 150), 50, cv::Scalar(255, 255, 255));
-        renderText(ft2, display, "59.6%", cv::Point(850, 200), 50, cv::Scalar(255, 255, 255));
+        renderText(ft2, display, temp_str, cv::Point(850, 150), 50, cv::Scalar(255, 255, 255));
+        renderText(ft2, display, humi_str, cv::Point(850, 200), 50, cv::Scalar(255, 255, 255));
         renderText(ft2, display, "오늘 주요 기사", cv::Point(50, 1850), 50, cv::Scalar(255, 255, 255));
 
         cv::imshow("Information Display", display);
